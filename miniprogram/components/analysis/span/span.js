560f1cbfdce7f0f713ecef8ebd1b4f49e75439e6
summerxixi <2235709694@qq.com>
2023-05-16 08:55:13 +0800

    fix:修改登录和my页面，缩进2tab

--- miniprogram/components/analysis/span/span.js
+++ miniprogram/components/analysis/span/span.js
@@ -1,24 +1,24 @@
 // components/analysis/span/span.js
 Component({
-    /**
-     * 组件的属性列表
-     */
-    properties: {
+  /**
+   * 组件的属性列表
+   */
+  properties: {
 
-    },
+  },
 
-    /**
-     * 组件的初始数据
-     */
-    data: {
+  /**
+   * 组件的初始数据
+   */
+  data: {
 
-    },
+  },
 
-    /**
-     * 组件的方法列表
-     */
-    methods: {
+  /**
+   * 组件的方法列表
+   */
+  methods: {
 
 
-    }
+  }
 })
--- miniprogram/pages/analysis/result/result.js
+++ miniprogram/pages/analysis/result/result.js
@@ -1,72 +1,72 @@
 // pages/analysis/analysis.js
 Page({
 
-    /**
-     * 页面的初始数据
-     */
-    data: {
-        current: 0,
-    },
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    current: 0,
+  },
 
-    handleActive(evt) {
-        console.log(evt.currentTarget.dataset.index)
-        this.setData({
-            current: evt.currentTarget.dataset.index
-        })
-    },
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad(options) {
+  handleActive(evt) {
+    console.log(evt.currentTarget.dataset.index)
+    this.setData({
+      current: evt.currentTarget.dataset.index
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
 
-    },
+  },
 
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady() {
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
 
-    },
+  },
 
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow() {
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
 
-    },
+  },
 
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide() {
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
 
-    },
+  },
 
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload() {
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
 
-    },
+  },
 
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh() {
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
 
-    },
+  },
 
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom() {
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
 
-    },
+  },
 
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage() {
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
 
-    }
+  }
 })
